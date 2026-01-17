0ed73e40f6ead9fd54f0ff31800b5f3d3f6cce01
Goncharov Anton <goncharrvx@icloud.com>
2026-01-17 17:03:14 +0300

    upd test

--- tests/android_app/test_open_wiki_article.py
+++ tests/android_app/test_open_wiki_article.py
@@ -1,14 +1,51 @@
 import allure
-from selene import browser
+import pytest
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+SEARCH_OPEN = (AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")
+SEARCH_INPUT = (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")
+RESULT_TITLE = (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")
+RESULT_CONTAINER = (AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_container")
 
 
-@allure.title("Open article from search results")
+@allure.feature("Поиск")
+@allure.story("Открытие статьи")
+@allure.title("Открыть первую статью из результатов поиска по запросу")
+@pytest.mark.android
 def test_open_article_from_search():
-    with allure.step("Open search"):
-        browser.element('//*[@text="Search Wikipedia"]').click()
+    query = "Selene"
 
-    with allure.step("Enter search query"):
-        browser.element('//*[@resource-id="org.wikipedia.alpha:id/search_src_text"]').type("Selene")
+    with allure.step("Открыть поиск"):
+        browser.element(SEARCH_OPEN).click()
 
-    with allure.step("Click on first search result"):
-        browser.element('//*[@resource-id="org.wikipedia.alpha:id/page_list_item_container"]').click()
+    with allure.step(f"Ввести запрос: {query}"):
+        browser.element(SEARCH_INPUT).type(query)
+
+    with allure.step("Проверить, что есть результаты"):
+        results = browser.all(RESULT_TITLE)
+        results.should(have.size_greater_than(0))
+
+    with allure.step("Открыть первый результат"):
+        browser.element(RESULT_CONTAINER).click()
+
+
+@allure.feature("Поиск")
+@allure.story("Поиск статьи")
+@allure.title("Поиск: результаты содержат введённый запрос")
+@pytest.mark.android
+def test_search():
+    query = "Appium"
+
+    with allure.step("Открыть поиск"):
+        browser.element(SEARCH_OPEN).click()
+
+    with allure.step(f"Ввести запрос: {query}"):
+        browser.element(SEARCH_INPUT).type(query)
+
+    with allure.step("Проверить, что найден хотя бы один результат"):
+        results = browser.all(RESULT_TITLE)
+        results.should(have.size_greater_than(0))
+
+    with allure.step(f"Проверить, что первый результат содержит: {query}"):
+        results.first.should(have.text(query))
